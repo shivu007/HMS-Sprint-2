60c1fc8010e490dec8adb6b1fd02e684907dc220
shivu007 <shivanigawade85@gmail.com>
2022-02-09 11:55:31 +0530

    models

--- HMSWebAPI/Models/Doctor.cs
+++ HMSWebAPI/Models/Doctor.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace HMSWebAPI.Models
 {
-    public class Doctor
+    public class DOCTOR
     {
         public string DID { get; set; }
         public string Dept { get; set; }
@@ -13,9 +13,9 @@ namespace HMSWebAPI.Models
         public string Username { get; set; }
 
 
-        //public virtual ICollection<APPOINTMENT> APPOINTMENTs { get; set; }
+        public virtual ICollection<APPOINTMENT> APPOINTMENTs { get; set; }
 
-        //public virtual ICollection<OPATIENT> OPATIENTs { get; set; }
+        public virtual ICollection<OPATIENT> OPATIENTs { get; set; }
         public virtual User User { get; set; }
     }
 }
