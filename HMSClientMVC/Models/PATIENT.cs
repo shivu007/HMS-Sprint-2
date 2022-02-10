d178ec6835c790b7a1b99c4ab57a776a9cc42c5a
PratikPawar1008 <pratik.pa08@gmail.com>
2022-02-10 16:27:08 +0530

    v54

--- HMSClientMVC/Models/PATIENT.cs
+++ HMSClientMVC/Models/PATIENT.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,7 @@ namespace HMSClientMVC.Models
 {
     public class PATIENT
     {
-
+        [Required]
         public string PID { get; set; }
         public string PName { get; set; }
         public string PGender { get; set; }
