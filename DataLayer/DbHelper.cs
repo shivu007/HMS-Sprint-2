b95f220a83067ade09ce3a24b7ec1d94027b1fbb
ChinmayKenny <chinmay.keny16@gmail.com>
2022-02-10 17:49:39 +0530

    a

--- DataLayer/DbHelper.cs
+++ DataLayer/DbHelper.cs
@@ -30,6 +30,7 @@ namespace DataLayer
         {
             try
             {
+
                 context.PATIENTs.Add(patient);
                 context.SaveChanges();
                 return true;
